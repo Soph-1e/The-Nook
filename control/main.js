420a86c69a0731def9bb80dc27401ff09b8a21c2
Soph-1e <lakes4@vcu.edu>
2026-05-08 23:05:29 -0400

    89guoihoiy809io

--- control/main.js
+++ control/main.js
@@ -1,8 +1,39 @@
-$("sets-btn").clock(
-    function(){
-        $(".black").addClass("show");
-        $(".sets-btn").hide();
-    }
-)
+$(".draggable").draggable({
+  revert: true,
+});
 
-$(".sets").draggable();
+$(".avatar").droppable({
+  accept: ".draggable",
+  drop: function (event, ui) {
+    if ($(ui.draggable).hasClass("pants")) {
+      $(ui.draggable).hide();
+      $(".avatar .pants").show();
+    }
+    if ($(ui.draggable).hasClass("shirt")) {
+      $(ui.draggable).hide();
+      $(".avatar .shirt").show();
+    }
+    if ($(ui.draggable).hasClass("hat")) {
+      $(ui.draggable).hide();
+      $(".avatar .hat").show();
+    }
+  },
+});
+
+$(".return").droppable({
+  accept: ".draggable",
+  drop: function (event, ui) {
+    if ($(ui.draggable).hasClass("pants")) {
+      $(ui.draggable).hide();
+      $(".return .pants").show();
+    }
+    if ($(ui.draggable).hasClass("shirt")) {
+      $(ui.draggable).hide();
+      $(".return .shirt").show();
+    }
+    if ($(ui.draggable).hasClass("hat")) {
+      $(ui.draggable).hide();
+      $(".return .hat").show();
+    }
+  },
+});
